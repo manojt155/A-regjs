39c74baa0366e121862941a1ab2ca5033684fdd3
manojt155 <manoj.tiwari@3pillarglobal.com>
2016-11-28 12:53:05 +0530

    DEMO

--- REGJS.js
+++ REGJS.js
@@ -1,104 +1,83 @@
 var express = require('express');
-var jwt = require('jsonwebtoken');
-var cookieParser = require('cookie-parser')
-
-var port = 3000;
 var app = express();
-
-var user = {
-username: 'foo',
-password: 'bar'
-};
-
-var tokenSettings = {
-alg: 'HS256'
-};
-
 app.disable('x-powered-by');
-app.use(cookieParser())
-
-app.get('/', function(req, res) {
-res.send('hey there');
+//evaluates input in eval() function
+app.get('/eval', function(req,res){
+var code = req.query.func;
+eval(code);
+res.send('Data evaluated.');
 });
 
-app.get('/login', function(req, res) {
-if (req.query.username == user.username &&
-req.query.password == user.password) {
-var token = jwt.sign(user, secret, {
-expiresIn: '5m',
-alg: 'HS256'
+//setTimeout function on the server side only accepts callback function and not a string
+app.get('/setTimeout', function(req,res){
+var code = req.query.func;
+setTimeout(loggin, 1000);
+res.send('Function set to execute on timeout.');
 });
 
-jwt.verify(req.cookies.token, 'test4',
-       {         
-     algorithms: ['HS256']
-       }, function (err, token) {
-     res.json(token);
-       });
+//setInterval function on the server side only accepts callback function and not a string
+app.get('/setInterval', function(req,res){
+var code = req.query.func;
+setInterval(loggin, 2000);
+res.send('Data set to execute every 2 seconds.');
+});
 
-res.cookie('token', token, {});
-res.send('you should have a cookie');
-} else {
-res.send('bad login');
+//setImmediate function on the server side only accepts callback function and not a string
+app.get('/immediate', function(req,res){
+var code = req.query.func;
+console.log("To execute "+ code);
+var immediateID = setImmediate(loggin, code);
+console.log("immediateID = "+immediateID);
+res.send('Data set to be executed immediately.');
+});
+
+//creating an anonymous function on the server side with an untrusted string
+app.get('/function1', function(req,res){
+var code = req.query.func;
+console.log("To execute "+ code);
+var myFunc1 = new Function(code);
+//Test payload: console.log("you have been hacked");
+console.log("Running func1: "+myFunc1());
+res.send('Function 1 executed');
+});
+
+//creating an anonymous function on the server side with arguments and an untrusted string for the body
+app.get('/function2', function(req,res){
+var code = req.query.func;
+console.log("To execute "+ code);
+var myFunc2 = new Function("arg1","arg2",code);
+//Test payload: console.log("you have been hacked");return arg1*arg2;
+console.log("Running func2: "+myFunc2(5, 6));
+
+res.send('Function 1 executed');
+});
+
+secret = 'SuperSecretPhrase';
+var context = new vm.createContext(sandbox);
+
+//passing untrusted input into the Script object and then calling it on a context
+app.get('/script', function(req,res){
+var code = req.query.func;
+//Test payload: console.log("secret: "+secret);
+var script = new vm.Script(code);
+for (var i = 0; i < 10; ++i) {
+//script.runInContext(context);
+script.runInThisContext();
 }
+res.send('Data evaluated 10 times.');
 });
 
-app.get('/test', function (req, res) {
-if (req.cookies.token) {
-jwt.verify(req.cookies.token, 'test4',
-{
-ignoreExpiration: false,
-algorithms: ['HS256']
-}, function (err, token) {
-res.json(token);
+//evaluating untrusted input using the vm without the Script object
+app.get('/vm', function(req,res){
+var code = req.query.func;
+//Test payload: console.log(secret);
+vm.runInDebugContext(code);
+res.send('Data evaluated once.');
 });
-} else {
-res.send('no token');
+
+function loggin(str) {
+console.log("123 - inside a function: "+str);
 }
-});
 
-app.get('/test2', function (req, res) {
-if (req.cookies.token) {
-jwt.verify(req.cookies.token, 'test4',
-{
-ignoreNotBefore: false,
-algorithms: ['HS256']
-}, function (err, token) {
-res.json(token);
-});
-} else {
-res.send('no token');
-}
-});
-
-app.get('/test3', function (req, res) {
-if (req.cookies.token) {
-jwt.verify(req.cookies.token, 'test4',
-{
-ignoreNotBefore: false,
-ignoreExpiration: false,
-algorithms: ['HS256']
-}, function (err, token) {
-res.json(token);
-});
-} else {
-res.send('no token');
-}
-});
-
-app.get('/test4', function (req, res) {
-if (req.cookies.token) {
-jwt.verify(req.cookies.token, 'test4',
-{
-algorithms: ['HS256']
-}, function (err, token) {
-res.json(token);
-});
-} else {
-res.send('no token');
-}
-});
-
-app.listen(port, () => {
-console.log("i'm listening...");
-});
+app.listen(3000);
+console.log("Server running on port 3000");
