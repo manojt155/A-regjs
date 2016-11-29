82ccf9802b49d8c8810e0d5f7191749b7fdca79f
manojt155 <manoj.tiwari@3pillarglobal.com>
2016-11-29 16:02:22 +0530

    DEMO

--- REGJS.js
+++ REGJS.js
@@ -16,26 +16,3 @@ var server = app.listen(3000, function() {
 var port = server.address().port;
 console.log('Your app listening at http://localhost:%s', port);
 });
-
-The insecure_script folder contains index.html file with the below code
-
-<html>
-<head>
-</head>
-<body>
-This is some text
-<p>
-This script doesn't fire the trigger
-<script src="js/stupid_client_script.js"></script>
-This script fires the trigger
-<script src="http://www.example.com/example.js"></script>
-<script src="http://www.wordpress.com/dod.js"></script>
-<script src="http://www.shoes.com/act.js"></script>
-This is the text src="http://www.example.com" in the HTML text, but not as part of script.
-</p>
-<p>
-<script src="https://www.example.com/securejs.js"></script>
-The last example is accessed over HTTPS and should be a "pat on the back."
-</p>
-</body>
-</html>
