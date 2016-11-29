c61edd3af225fa704040d63e4da59c2021ec2563
manojt155 <manoj.tiwari@3pillarglobal.com>
2016-11-29 16:05:26 +0530

    DEMO

--- REGJS.js
+++ REGJS.js
@@ -1,18 +1,52 @@
-var express = require('express');
-var app = express();
-
-app.disable('X-Powered-By');
-
-// Register templating engine
-app.engine('html', require('ejs').renderFile);
-app.set("view engine", "html");
-app.set("views", __dirname + "/insecure_script");
-
-app.get('/', function(req, res) {
-res.sendFile('index');
+document.ready(function () {
+console.log("Hello World!");
 });
 
-var server = app.listen(3000, function() {
-var port = server.address().port;
-console.log('Your app listening at http://localhost:%s', port);
-});
+The .html code is as below:
+
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="index.js"></script>
+<script>
+// Trigger 1 - Math.Random
+function makeRandom1() {
+var min = 1;
+var max = 10;
+// 1st HIT
+return Math.floor(Math.random() * (max - min) + min);
+}
+
+    // Trigger 2 - RandomSource.getRandomValues test
+    function makeRandom2() {
+        var array = new Uint32Array(1);
+        // 2nd HIT
+        var rnd2 = window.crypto.getRandomValues(array);
+        // 3rd HIT
+        var rnd3 = crypto.getRandomValues([]);
+        console.log("window.crypto.getRandomValues(string)");
+        return rnd2[0] + rnd3[1];
+    }
+</script>
+<meta name="description" content="Free Web tutorials">
+<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+<script src="//www.google-analytics.com/analytics.js"></script>
+<meta name="author" content="Hege Refsnes">
+<script>
+    $(document).ready(function() {
+        $('#usrfld').val('userid' + makeRandom1());
+        $.ajax({
+            type: "POST",
+            url: '/submit',
+            data: {
+                "usrfld": "userid" + makeRandom2()
+            },
+            success: null,
+            dataType: 'json'
+        });
+    });
+</script>
+</head>
+<body>
+</body>
+</html>
