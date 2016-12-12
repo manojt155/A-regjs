3451bde03e14d3cf14e92604767e2b1d68264528
manojt155 <manoj.tiwari@3pillarglobal.com>
2016-12-12 13:13:40 +0530

    demo

--- REGJS.js
+++ REGJS.js
@@ -14,7 +14,7 @@ register: Scooter,
 options: {}
 },{
 register: Blankie,
-options: {defaultSrc: 'http:'}
+options: {defaultSrc: 'none'}
 }], 
 function (err) {
 if (err) {
@@ -26,7 +26,7 @@ server.route({
 method: 'GET',
 path: '/',
 handler: function (request, reply) {
-reply('Test negative for defaultSrc http:');
+reply('Test postive for defaultSrc');
 }
 });
 
