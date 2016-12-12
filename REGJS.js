29e0d0e222d4f329cbaa6900c98ee0317708e520
manojt155 <manoj.tiwari@3pillarglobal.com>
2016-12-12 12:38:47 +0530

    demo

--- REGJS.js
+++ REGJS.js
@@ -2,4 +2,9 @@ var express = require('express');
 var app = express();
 app.disable('x-powered-by');
 
-app.locals.myvalue = '123';
+app.get('/command', function(req, res){
+if(req.params.input=="true")
+app.locals.myvalue = "1";
+else if (req.params.input=="false")
+app.locals.myvalue = "0";
+});
