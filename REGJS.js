3ebc4744e23874f15486bbeb8cac9090b1da6387
manojt155 <manoj.tiwari@3pillarglobal.com>
2016-12-12 13:05:32 +0530

    demo

--- REGJS.js
+++ REGJS.js
@@ -1,19 +1,10 @@
-'use strict';
-
-const Hapi = require('hapi');
-const myCrypto = require('crypto');
-const Joi = require('joi');
+var Hapi = require('hapi');
 var Blankie = require('blankie');
 var Scooter = require('scooter');
 const Inert = require('inert');
-const server = new Hapi.Server();
 
-var workFactor = 10000;
-
-server.app.key = 'secret_app_value_102';
-server.connection({
-port: 3000
-});
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
 
 server.register([{
 register: Inert,
@@ -23,31 +14,21 @@ register: Scooter,
 options: {}
 },{
 register: Blankie,
-options: {scriptSrc: 'self'}
-}], 
-function (err) {
+options: {childSrc: '*'}
+}], function (err) {
 if (err) {
 throw err;
 }
 });
 
 server.route({
-method: 'POST',
-path: '/positive/pbkdf2/variable/{password*}',
-config: {
-validate: {
-params: {
-password: Joi.string().max(128).min(8).alphanum()
-}
-},
+method: 'GET',
+path: '/',
 handler: function (request, reply) {
-const salt = myCrypto.randomBytes(256).toString('hex');
-myCrypto.pbkdf2(request.params.password, salt, workFactor, 512, 'sha512', function (err, hash) {
-if (err) throw err;
-reply(hash.toString('base64'));
-});
-}
+reply('Test negative for childSrc');
 }
 });
 
-server.start(function () {});
+server.start(function () {
+console.log('info', 'Server running at: ' + server.info.uri);
+});
